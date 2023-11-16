481c1678ebc029dd2c0384638a6e8c7b51c2722e
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2023-11-16 15:36:26 +0500

    Add files via upload

--- register new base function.c
+++ register new base function.c
@@ -1,21 +1,54 @@
+#include<stdio.h>
+#include<time.h>
+#include<unistd.h>
+
 void registernewbase()
+
+
 {
-	char rank[50], officialname[50], baselocation[50], rankoption;
-	printf("enter your rank: ");
-	printf("(1)colonel --> c\n(2)lieutenant --> l\n(3)major --> m");
-	fgets(rank, sizeof(rank), stdin);
-	if(rankoption != 'c' || rankoption != 'l' || rankoption != 'm' )
+	char rank[50], officialname[50], baselocation[50], destination[50];
+	char rankoption;
+	int baseduration, base_strength;
+	printf("\nEnter your rank: ");
+	printf("\n1. Colonel ---> C\n2. Lieutenant ---> L\n3. Major ---> M\n");
+	scanf("%c", &rankoption);
+	// printf("enter your name: ");
+	// fgets(officialname, sizeof(officialname), stdin);
+	// printf("location of base: ");
+	// fgets(baselocation, sizeof(baselocation), stdin);
+	// printf("your bease has been registered");
+	switch (rankoption)
 	{
-		printf("wrong ranked entered or you are not eligible for base registration");
-		//apply delay functioon here of 5 seconds and then go back to menu//
-		menu();
+	case 'C':
+	case 'c':
+	case 'L':
+	case 'l':
+	case 'M':
+	case 'm':
+		printf("\nEnter your name: ");
+		scanf("%s", officialname);
+		printf("\nEnter the location of your base: ");
+		scanf("%s", baselocation);
+		printf("\nEnter the base duration(in minutes): ");
+		scanf("%d", &baseduration);
+		printf("\nEnter the strength of your base: ");
+		scanf("%d", &base_strength);	
+		printf("\nEnter the destination of your base: ");
+		scanf("%s", destination);
+		printf("\nYour base has been registered.");
+		break;
+	
+	default:
+		printf("\nInvalid option");
+		registernewbase();
 	}
-	printf("enter your name: ");
-	fgets(officialname, sizeof(officialname), stdin);
-	printf("location of base: ");
-	fgets(baselocation, sizeof(baselocation), stdin);
-	//have to apply filing function in this which will store name,tank,id,password,
-	printf("your bease has been registered");
-	
-	
+}
+
+
+int main(){
+	registernewbase();
+	//apply delay functioon here of 5 seconds and then go back to menu//
+	sleep(5);
+	// menu();
+	return 0;
 }
